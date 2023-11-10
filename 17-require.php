79d26696f18782e891778854b09293735457c53c
Cristianps1988 <cristianps_1@hotmail.com>
2023-11-10 06:28:06 -0500

    require, includes, json

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,19 @@
 <?php include 'includes/header.php';
 
+// el include me sirve para reutilizar contenido y mostrarlo
+// si hago referencia a un archivo que no existe, me muestra un error y se crashea la página
+
+echo "Después del include";
+
+// revisa si ya se incluyó el archivo, si no lo incluye... si ya se incluyó, ignora esta línea
+include_once 'includes/header.php';
+
+// cuando utilizar include o require?
+// utilizar require para funciones e include para páginas
+require 'funciones.php';
+echo "<br>";
+iniciarApp();
 
 
-
-
-
+// require hace lo mismo que include, pero si hay un error, me muestra el error y se detiene la ejecución del script    
 include 'includes/footer.php';
--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,54 @@
 <?php include 'includes/header.php';
 
 
+$products = [
+    [
+        "name" => "Laptop",
+        "price" => 1000,
+        "avaible" => true
+    ],
+    [
+        "name" => "Televisión",
+        "price" => 600,
+        "avaible" => false
+    ],
+    [
+        "name" => "Radio",
+        "price" => 100,
+        "avaible" => true
+    ],
+    [
+        "name" => "Iphone",
+        "price" => 1100,
+        "avaible" => true
+    ],
+    [
+        "name" => "Tablet",
+        "price" => 800,
+        "avaible" => false
+    ]
+];
 
+echo "<prev>";
+var_dump($products);
+echo "</prev>";
 
+echo "<br>";
 
+// json_encode convierte un array a json (string)
+$json = json_encode($products, JSON_UNESCAPED_UNICODE); // JSON_UNESCAPED_UNICODE para que no se transformen las tildes a unicode
+
+echo "<prev>";
+var_dump($json);
+echo "</prev>";
+
+echo "<br>";
+
+// json_decode convierte un json a array
+$jsonArray = json_decode($json);
+
+echo "<prev>";
+var_dump($jsonArray);
+echo "</prev>";
 
 include 'includes/footer.php';
